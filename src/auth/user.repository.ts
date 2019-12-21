cceeea3d252d84ae98365e4a9eb273a7a8ed398d
Katie Lamber <katielamber02@gmail.com>
2019-12-21 13:55:44 +0200

    23505 error conflict

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,10 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredetialDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 // Here should be heavy db related logic so that we don't have to write it in service
 @EntityRepository(User)
@@ -8,10 +12,22 @@ export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredetialDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    // we could use find One and then save but we will have 2 queries to db
+    // const exists=this.findOne({username})
+
     const user = new User();
     user.username = username;
     user.password = password;
 
-    await user.save();
+    try {
+      await user.save();
+    } catch (error) {
+      console.log(error.code); //
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
