d084a11a912913615343abc7f54b7c2bb0db6795
Katie Lamber <katielamber02@gmail.com>
2019-12-21 13:39:17 +0200

    password validation added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 
 import { AuthCredetialDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
@@ -8,7 +8,9 @@ export class AuthController {
   // DI: in order to use the service
   constructor(private authService: AuthService) {}
   @Post('/signup')
-  signUp(@Body() authCredentialsDto: AuthCredetialDto): Promise<void> {
+  signUp(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredetialDto,
+  ): Promise<void> {
     console.log(authCredentialsDto);
     return this.authService.signUp(authCredentialsDto);
   }
