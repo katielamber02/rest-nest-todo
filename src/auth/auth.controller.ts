d648569239936de1957648e962bde556f11034d2
Katie Lamber <katielamber02@gmail.com>
2019-12-21 17:56:41 +0200

    jwt added and test request with @UseGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credentials.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -15,7 +23,15 @@ export class AuthController {
     return this.authService.signUp(authCredentialsDto);
   }
   @Post('/signin')
-  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialDto) {
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@Req() req) {
+    console.log(req);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,19 +2,28 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credentials.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialDto) {
+  async signIn(
+    authCredentialsDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
+    console.log(
+      'NAME:',
+      await this.userRepository.validateUserPassword(authCredentialsDto),
+    );
     const username = await this.userRepository.validateUserPassword(
       authCredentialsDto,
     );
@@ -22,5 +31,11 @@ export class AuthService {
     if (!username) {
       throw new UnauthorizedException('Invalid credentials'); // for both username or password
     }
+
+    const payload: JwtPayload = { username };
+    const accessToken = await this.jwtService.sign(payload);
+    console.log('accessToken:', accessToken);
+
+    return { accessToken };
   }
 }
